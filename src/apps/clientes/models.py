bfc998c100d1d7925f6e4a51858c5c86f3a8a621
wend.devdesigner@gmail.com <wend.devdesigner@gmail.com>
2025-08-01 20:04:14 -0300

    add: cliente

--- src/apps/clientes/models.py
+++ src/apps/clientes/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
-# Create your models here.
+class Cliente(models.Model):
+    nome = models.CharField(verbose_name="Nome", max_length=65, null=True, blank=True)
+    telefone = models.CharField(verbose_name="Telefone", max_length=15, null=True, blank=True)
+
+    def __str__(self):
+        return f"{self.id} - {self.nome}"
--- src/apps/clientes/urls.py
+++ src/apps/clientes/urls.py
@@ -1,1 +1,7 @@
 from django.urls import path
+from apps.clientes.views import clientes_list, clientes_detail
+
+urlpatterns = [
+    path("clientes/", clientes_list),
+    path("clientes/<int:pk>/", clientes_detail)
+]
