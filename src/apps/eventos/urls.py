1cb94c5f1f318d2dc2e76f885210f7eaf9b45c5e
wend.devdesigner@gmail.com <wend.devdesigner@gmail.com>
2025-08-04 23:06:42 -0300

    add: put and delete evento

--- src/apps/eventos/urls.py
+++ src/apps/eventos/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from apps.eventos.views import eventos_list
+from apps.eventos.views import eventos_list, eventos_detail
 
 urlpatterns = [
     path("eventos/", eventos_list),
+    path("eventos/<int:pk>/", eventos_detail),
 ]
--- src/apps/eventos/views.py
+++ src/apps/eventos/views.py
@@ -17,3 +17,24 @@ def eventos_list(request):
             serializer.save()
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+@api_view(["GET", "PUT", "DELETE"])
+def eventos_detail(request,pk):
+    try:
+        evento = Evento.objects.get(pk=pk)
+    except Evento.DoesNotExist:
+        return Response(status=status.HTTP_404_NOT_FOUND)
+
+    if request.method == "GET":
+        serializer = EventoReadSerializer(evento)
+        return Response(serializer.data)
+
+    elif request.method == "PUT":
+        serializer = EventoWriteSerializer(evento, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+
+    elif request.method == "DELETE":
+        evento.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
